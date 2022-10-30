f921a10b386e9617174bc449d4106af7aeeee318
meetwq <meetwq@gmail.com>
2022-10-30 21:35:55 +0800

    添加帮助图片

--- nonebot_plugin_imagetools/__init__.py
+++ nonebot_plugin_imagetools/__init__.py
@@ -1,7 +1,10 @@
+import math
 from io import BytesIO
-from typing import Union
+from typing import List, Union
+from PIL.Image import Image as IMG
 
 from nonebot.params import Depends
+from nonebot.utils import run_sync
 from nonebot.matcher import Matcher
 from nonebot.typing import T_Handler
 from nonebot import on_command, require
@@ -9,6 +12,7 @@ from nonebot.plugin import PluginMetadata
 from nonebot.adapters.onebot.v11 import Message, MessageSegment
 
 require("nonebot_plugin_imageutils")
+from nonebot_plugin_imageutils import BuildImage, Text2Image
 
 from .utils import Command
 from .data_source import commands
@@ -24,6 +28,45 @@ __plugin_meta__ = PluginMetadata(
         "version": "0.1.1",
     },
 )
+
+
+help_cmd = on_command("图片操作", aliases={"图片工具"}, block=True, priority=12)
+
+
+@run_sync
+def help_image() -> BytesIO:
+    def cmd_text(commands: List[Command], start: int = 1) -> str:
+        texts = []
+        for i, meme in enumerate(commands):
+            text = f"{i + start}. " + "/".join(meme.keywords)
+            texts.append(text)
+        return "\n".join(texts)
+
+    head_text = "简单图片操作，支持的操作："
+    head = Text2Image.from_text(head_text, 30, weight="bold").to_image(padding=(20, 10))
+
+    imgs: List[IMG] = []
+    col_num = 2
+    num_per_col = math.ceil(len(commands) / col_num)
+    for idx in range(0, len(commands), num_per_col):
+        text = cmd_text(commands[idx : idx + num_per_col], start=idx + 1)
+        imgs.append(Text2Image.from_text(text, 30).to_image(padding=(20, 10)))
+    w = max(sum((img.width for img in imgs)), head.width)
+    h = head.height + max((img.height for img in imgs))
+    frame = BuildImage.new("RGBA", (w, h), "white")
+    frame.paste(head, alpha=True)
+    current_w = 0
+    for img in imgs:
+        frame.paste(img, (current_w, head.height), alpha=True)
+        current_w += img.width
+    return frame.save_jpg()
+
+
+@help_cmd.handle()
+async def _():
+    img = await help_image()
+    if img:
+        await help_cmd.finish(MessageSegment.image(img))
 
 
 def create_matchers():
