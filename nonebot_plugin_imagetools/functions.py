f5efefbd268e3346a415e984e9f0d615d646ab8b
jcjrobert <jcjrobbie@gmail.com>
2022-11-09 20:46:00 +0800

    add gif_obverse_reverse

--- nonebot_plugin_imagetools/functions.py
+++ nonebot_plugin_imagetools/functions.py
@@ -152,6 +152,29 @@ def gif_reverse(img: BuildImage = Img(), arg=NoArg()):
         frames[0].info["transparency"] = transparency
         return save_gif(frames, duration)
 
+def gif_obverse_reverse(img: BuildImage = Img(), arg=NoArg()):
+    image = img.image
+    if getattr(image, "is_animated", False):
+        duration = image.info["duration"] / 1000
+        frames: List[IMG] = []
+        for i in range(image.n_frames):
+            image.seek(i)
+            frames.append(image.convert("RGB"))
+        frames = frames + frames[::-1]
+        return save_gif(frames, duration)
+
+
+def gif_reverse_obverse(img: BuildImage = Img(), arg=NoArg()):
+    image = img.image
+    if getattr(image, "is_animated", False):
+        duration = image.info["duration"] / 1000
+        frames: List[IMG] = []
+        for i in range(image.n_frames):
+            image.seek(i)
+            frames.append(image.convert("RGB"))
+        frames = frames[::-1] + frames
+        return save_gif(frames, duration)
+
 
 def gif_split(img: BuildImage = Img(), arg=NoArg()):
     image = img.image
